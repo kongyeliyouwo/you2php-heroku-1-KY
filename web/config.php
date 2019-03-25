fa94662d1190278c9cd4ff571657370a9bf0e237
kongyeliyouwo <36878970+kongyeliyouwo@users.noreply.github.com>
2019-03-25 17:33:54 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDZnujV2PYHBEt9_wemKXGr_h1-gLhtA_g');
+define('APIKEY', 'AIzaSyDZnujV2t9_wem1-gLhtA_g');
 define('GJ_CODE', $gl);
 define('SITE_NAME', 'ytb');
 define('TITLENAME', 'ytb');
-define('EN2DEKEY', 'fGXe3R9OH7');
-define('EMAIL', '1070604045@qq.com');
+define('EN2DEKEY', 'fGXH7');
+define('EMAIL', '10740m');
 ?>
